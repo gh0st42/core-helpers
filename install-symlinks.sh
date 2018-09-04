e6df18cc8b79df64866bca2fcf00087876232955
lbaumgaertner <lbaumgaertner@mathematik.uni-marburg.de>
2018-09-04 14:05:16 +0200

    added g(enerate)f(file) tool and added exit after -h help for all commands

--- install-symlinks.sh
+++ install-symlinks.sh
@@ -4,3 +4,4 @@ ln -s `pwd`/bin/ccc /usr/local/bin/ccc
 ln -s `pwd`/bin/cda /usr/local/bin/cda
 ln -s `pwd`/bin/cea /usr/local/bin/cea
 ln -s `pwd`/bin/cpa /usr/local/bin/cpa
+ln -s `pwd`/bin/gf /usr/local/bin/gf
