6b2c1ec29920eba60c54dc57c6497f14cbf34b0d
gh0st <1264131+gh0st42@users.noreply.github.com>
2021-08-11 18:23:01 +0200

    added cexec tool, speed up cda, sorted node list in c*a tools

--- install-symlinks.sh
+++ install-symlinks.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ln -s `pwd`/bin/cbash /usr/local/bin/cbash
+ln -s `pwd`/bin/cexec /usr/local/bin/cexec
 ln -s `pwd`/bin/ccc /usr/local/bin/ccc
 ln -s `pwd`/bin/cda /usr/local/bin/cda
 ln -s `pwd`/bin/cea /usr/local/bin/cea
